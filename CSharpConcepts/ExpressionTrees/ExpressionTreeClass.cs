764d9d1d8e9c7fb1c1481bbd773da8c6b3fd99ea
Andrei Leonte <leonteiandrei@gmail.com>
2025-03-19 13:11:53 +0200

    Added expression trees

---
+++ CSharpConcepts/ExpressionTrees/ExpressionTreeClass.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionTrees
+{
+    public class ExpressionTreeClass
+    {
+        public static void RunBasicExpression()
+        {
+            // Step 1: Create the expression tree
+            Expression<Func<int, int>> addFive = (num) => num + 5;
+
+            // Step 2: Compile the expression tree into a delegate
+            Func<int, int> compiledExpression = addFive.Compile();
+
+            // Step 3: Invoke the delegate to execute the expression
+            int result = compiledExpression(10); // Pass the argument 10
+
+            // Output the result
+            Console.WriteLine(result); // Output will be 15            
+        }
+
+        public static void RunFromLeaf()
+        {
+            // Step 1: Create the expression tree
+            var one = Expression.Constant(1, typeof(int));
+            var two = Expression.Constant(2, typeof(int));
+            var addition = Expression.Add(one, two);
+
+            // Step 2: Compile the expression tree into a delegate
+            var lambda = Expression.Lambda<Func<int>>(addition);
+            var compiledExpression = lambda.Compile();
+
+            // Step 3: Invoke the delegate to execute the expression
+            int result = compiledExpression();
+
+            // Output the result
+            Console.WriteLine(result); // Output will be 3
+        }
+    }
+}
---
+++ CSharpConcepts/ExpressionTrees/LinqQueries.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ExpressionTrees
+{
+    public class LinqQueries
+    {
+        public static void Run()
+        {
+            using var context = new AppDbContext();
+
+            SeedDatabase(context);
+
+            var people = context.People.ToList();
+            
+            foreach (var person in people)
+            {
+                var books = GetBooksBasedOnAge(context.Books, person.Age).ToList();
+
+                Console.WriteLine($"Person {person.Name} with age {person.Age} can get numner of books {books.Count}");
+            }
+        }
+
+        static IQueryable<Book> GetBooksBasedOnAge(IQueryable<Book> books, int age)
+        {
+            if (age > 18)
+            {
+                return books;
+            }
+            else
+            {
+                ParameterExpression param = Expression.Parameter(typeof(Book), "b");
+                MemberExpression property = Expression.Property(param, nameof(Book.IsForAdults));
+                ConstantExpression constant = Expression.Constant(false);
+                BinaryExpression body = Expression.Equal(property, constant);
+                Expression<Func<Book, bool>> expression = Expression.Lambda<Func<Book, bool>>(body, param);
+
+                return books.Where(expression);
+            }
+        }
+
+        static void SeedDatabase(AppDbContext context)
+        {
+            var books = new List<Book>
+            {
+                new(1, "Book for Kids", false ),
+                new (2, "Book for Teens", false ),
+                new (3, "Book for Adults", true ),
+                new (4, "Another Book for Adults", true )
+            };
+
+            context.Books.AddRange(books);
+
+            var people = new List<Person>
+            {
+                new (1, "John", 17 ),
+                new (2, "Jane", 20 )
+            };
+
+            context.People.AddRange(people);
+
+            context.SaveChanges();
+        }
+    }
+
+
+    public class Person(
+        int id, string name, int age)
+    {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public int Age { get; set; } = age;
+    }
+
+    public class Book(int id, string title, bool isForAdults)
+    {
+        public int Id { get; set; } = id;
+        public string Title { get; set; } = title;
+        public bool IsForAdults { get; set; } = isForAdults;
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("InMemoryDb");
+        }
+    }
+}
