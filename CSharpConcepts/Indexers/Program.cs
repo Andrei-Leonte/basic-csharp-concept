02e7e793555b53507189d1362f8cb1946e30e096
Andrei Leonte <leonteiandrei@gmail.com>
2025-03-19 13:34:07 +0200

    Added indexers

---
+++ CSharpConcepts/Indexers/Program.cs
@@ -0,0 +1,11 @@
+﻿using Indexers;
+
+BookIndexer<Book> books = new BookIndexer<Book>();
+
+books[0] = new Book("author", "name");
+
+//Throw custom exception
+books[1] = new Book("author", "name");
+
+
+Console.ReadLine();
